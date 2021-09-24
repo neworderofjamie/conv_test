63634bd6ebb243dc92ad9ceee7477c37433c7eec
neworderofjamie <neworderofjamie@gmail.com>
2021-09-24 12:15:41 +0100

    show both currents

--- show_out_currents.py
+++ show_out_currents.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
-out_currents = np.fromfile("outCurrents%s.bin" % sys.argv[1], dtype=np.float32)
-print(np.amin(out_currents), np.amax(out_currents))
-out_currents = out_currents.reshape((62, 62))
-plt.imshow(out_currents)
+out_currents_procedural = np.fromfile("outCurrentsProcedural.bin", dtype=np.float32)
+out_currents_toeplitz = np.fromfile("outCurrentsToeplitz.bin", dtype=np.float32)
 
+out_currents_procedural = out_currents_procedural.reshape((62, 62))
+out_currents_toeplitz = out_currents_toeplitz.reshape((62, 62))
+
+fig, axes = plt.subplots(2)
+axes[0].set_title("Procedural")
+axes[0].imshow(out_currents_procedural)
+
+axes[1].set_title("Toeplitz")
+axes[1].imshow(out_currents_toeplitz)
+
+fig.tight_layout(pad=0)
 plt.show()
